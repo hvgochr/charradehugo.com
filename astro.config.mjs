0d673a3eb75b8e4fbb29389723afef5b3e8200b6
Hugo Ch <charrade.hugo@gmail.com>
2026-01-28 23:10:45 +0100

    Add astro react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
 import { defineConfig, fontProviders } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  integrations: [react()],
   experimental: {
     fonts: [
       {
