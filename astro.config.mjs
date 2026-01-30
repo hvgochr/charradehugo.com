2c5a1d3b1afc1393cfc3c4f6fd7dae54c4d48296
Hugo Ch <charrade.hugo@gmail.com>
2026-01-30 01:51:45 +0100

    Add astro mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@
 import { defineConfig, fontProviders } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [react()],
+  integrations: [react(), mdx()],
   experimental: {
     fonts: [
       {
