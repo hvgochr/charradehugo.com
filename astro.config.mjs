0553ba9392460ba87304a7cba99e65abd07fa5b6
Hugo Ch <charrade.hugo@gmail.com>
2026-02-08 18:46:26 +0100

    Install Astro sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,14 @@ import { defineConfig, fontProviders } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [react(), mdx()],
+  integrations: [react(), mdx(), sitemap()],
   experimental: {
     fonts: [
       {
